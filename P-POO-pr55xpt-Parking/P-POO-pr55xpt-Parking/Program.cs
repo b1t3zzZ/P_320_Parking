5ea0e5d4917aebff9bd9d753beafd15e1c6d2924
b1t3zzZ <tsibulevskiym@gmail.com>
2026-08-28 11:24:45 +0200

    doc(Rapport):Il reste 3.4 à faire

---
+++ P-POO-pr55xpt-Parking/P-POO-pr55xpt-Parking/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace P_POO_pr55xpt_Parking
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
